d93b58b87bbfe69e461b3e3b4e3e24f6e42bf16d
aniltarhan <anil.tarhan@gmail.com>
2022-03-17 18:18:24 +0300

    Proje dosyası eklendi.

---
+++ src/Giris/DaireninAlani.java
@@ -0,0 +1,24 @@
+package Giris;
+import java.util.Scanner;
+public class DaireninAlani {
+    public static void main(String[] args) {
+      int r;
+      double pi = 3.14;
+
+        System.out.print("Yarıçapı giriniz : ");
+
+
+      Scanner input = new Scanner(System.in);
+      r = input.nextInt();
+      double alan = pi * r * r;
+      double cevre = 2 * pi * r ;
+
+        System.out.println("Dairenin Alanı : " + alan);
+        System.out.println("Dairenin Çevresi : " + cevre);
+
+
+
+
+
+    }
+}
